fa1ec06d5ab798f9843df825f55d5b571bbc3fda
foo-dogsquared <christiangabrielarazas@gmail.com>
2019-03-21 22:35:02 +0800

    Change the module entry point

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const fs = require("fs");
 const path = require("path");
 
-const freebiesHuntJSON = fs.readFileSync(path.resolve(__dirname, "./api/freebies.json"), "utf8");
+const data = fs.readFileSync(path.resolve(__dirname, "./api/freebies.json"), "utf8");
+const schema = fs.readFileSync(path.resolve(__dirname, "./api/freebies.schema.json"), "utf8");
+
+const freebiesHuntJSON = {
+  data,
+  schema
+};
 
 module.exports = freebiesHuntJSON;
