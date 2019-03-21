cf0f6d10b66b5f9e589ba6117921e6c856be21c7
foo-dogsquared <christiangabrielarazas@gmail.com>
2019-03-22 00:29:41 +0800

    Fixed the entry points

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
-const data = fs.readFileSync(path.resolve(__dirname, "./api/freebies.json"), "utf8");
-const schema = fs.readFileSync(path.resolve(__dirname, "./api/freebies.schema.json"), "utf8");
+const data = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./api/freebies.json"), "utf8"));
+const schema = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./api/freebies.schema.json"), "utf8"));
 
 const freebiesHuntJSON = {
   data,
