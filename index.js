fb77b5bdf74fecda694f551c982a9e715f415dee
foo-dogsquared <christiangabrielarazas@gmail.com>
2019-03-23 22:34:48 +0800

    Update the module with the categorized freebies and its updated version

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const path = require("path");
 
 const data = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./api/freebies.json"), "utf8"));
 const schema = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./api/freebies.schema.json"), "utf8"));
+const categorizedData = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./api/freebies.categorized.json"), "utf8"));
+const categorizedDataSchema = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./api/freebies.categorized.schema.json"), "utf8"));
 
 const freebiesHuntJSON = {
   data,
-  schema
+  schema,
+  categorizedData,
+  categorizedDataSchema,
 };
 
 module.exports = freebiesHuntJSON;
